abf006db28cc0646ac48ef3eb3f50513628cf918
cgmcname <periclescgm@live.com>
2017-04-06 13:32:29 -0500

    Shopping cart

---
+++ src/main/java/edu/uark/models/entities/ShoppingCart.java
@@ -0,0 +1,16 @@
+package edu.uark.models.entities;
+import java.util.ArrayList;
+public Class ShoppingCart
+{
+	ArrayList cart;
+	int count;
+	int totalPrice; 
+	ShoppingCart()
+	{
+	  cart = new ArrayList();
+	  count = 0;
+	  totalPrice = 0;
+	}
+	
+
+}
